2119a24a215b252249bc2630be78ef17c5780fcf
root <root@localhost>
2019-01-07 16:42:31 -0500

    #574 - A previous commit (a403182) was bad. The file was moved to the wrong directory. I am moving it to the right directory now.

---
+++ Tests/ApiStateEnforcer/Test_ApiStateEnforcer.py
@@ -0,0 +1,80 @@
+from unittest import TestCase
+
+from ForemanApiWrapper.ForemanApiWrapper import ForemanApiWrapper
+from ApiStateEnforcer import ApiStateEnforcer
+
+class Test_ApiStateEnforcer(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(Test_ApiStateEnforcer, self).__init__(*args, **kwargs)
+        self.username = "admin"
+        self.password = "password"
+        self.url = "https://15.4.7.1"
+        self.verifySsl = False
+
+    def test_Check_RecordExists_Success(self):
+
+        apiWrapper = ForemanApiWrapper(self.username, self.password, self.url, self.verifySsl)
+        apiStateEnforcer = ApiStateEnforcer(apiWrapper)
+
+        endpoint = "/api/environments/some_environment"
+        method = "get"
+
+        jsonState = apiStateEnforcer.Check(endpoint, method)
+
+    def test_Compare_MinimalStateExists_Success(self):
+
+        apiWrapper = ForemanApiWrapper(self.username, self.password, self.url, self.verifySsl)
+        apiStateEnforcer = ApiStateEnforcer(apiWrapper)
+
+        endpoint = "/api/environments"
+        method = "get"
+
+        actualState = apiStateEnforcer.Check(endpoint, method)
+        total = actualState["total"]
+
+        minimalState = { "total" : total}
+
+        minimalStateExists = apiStateEnforcer.Compare(minimalState, actualState)
+
+        self.assertTrue(minimalStateExists)
+
+    def test_Set_CreateRecord_Success(self):
+
+        apiWrapper = ForemanApiWrapper(self.username, self.password, self.url, self.verifySsl)
+        apiStateEnforcer = ApiStateEnforcer(apiWrapper)
+
+        endpoint = "/api/environments"
+        method = "post"
+        minimalState =  {
+            "environment": {
+                "name": "some_environment"
+            }
+        }
+
+        actualState = apiStateEnforcer.Set(endpoint, method, minimalState)
+
+        self.assertIsNotNone(actualState)
+        self.assertEqual(type(actualState), dict)
+
+    def test_Delete_DeleteRecord_Success(self):
+
+        apiWrapper = ForemanApiWrapper(self.username, self.password, self.url, self.verifySsl)
+        apiStateEnforcer = ApiStateEnforcer(apiWrapper)
+
+        environmentName = "some_environment"
+
+        endpoint = "/api/environments/{0}".format(environmentName)
+        method = "delete"
+        httpMethodArguments =  {
+            "environment": {}
+        }
+
+        minimalState = {
+            "name": environmentName,
+        }
+
+        actualState = apiStateEnforcer.Delete(endpoint, method, httpMethodArguments, minimalState)
+
+        self.assertIsNotNone(actualState)
+        self.assertEqual(type(actualState), dict)
