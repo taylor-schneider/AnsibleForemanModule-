cf90b9f59eb2017c9fadd73daa2b5c5681781c76
tschneider <tschneider@live.com>
2019-01-07 13:34:12 -0500

    #574 - Implemented Check, Compare, and Set functions. Updated the ForemanAppiWrapper to support specifying HTTP headers while making requests.

---
+++ ApiStateEnforcer.py
@@ -0,0 +1,78 @@
+from ApiStateEnforcerException import ApiStateEnforcerException
+
+class ApiStateEnforcer():
+
+    def __init__(self, apiWrapper):
+        self.apiWrapper = apiWrapper
+
+    SetCompareFailedErrorMessage = "The record returned from the set operation did not represent the specified minimal state."
+
+    def Check(self, checkUrl, httpMethod):
+
+        return self.apiWrapper.MakeApiCall(checkUrl, httpMethod)
+
+    def Compare(self, minimalState, actualState):
+
+        # This function will return true or false based on whether or not the
+        # actual state represents the minimal state
+        #       Ie. All the keys/values in minimal state exist in actual
+
+        # Check that the two objects are the same type
+        if type(minimalState) != type(actualState):
+            return False
+
+        # Now that we know the objects are the same, we can compare
+
+        # Certain non primitives need to be handled separately
+
+        # Dictionaries are an axample of this
+        if isinstance(minimalState, dict):
+            actualKeys = list(actualState.keys())
+
+            # If a key is missing that is a dead givaway
+            for key,value in minimalState.items():
+                if key not in actualKeys:
+                    return False
+
+                # Call this function recursively
+                # Return false if any of the keys dont match
+
+                actualValue = actualState[key]
+                minimalValue = minimalState[key]
+
+                comparisonResult = self.Compare(minimalValue, actualValue)
+                if not comparisonResult:
+                    return False
+
+            # If we got here without exiting, we match!
+            return True
+
+        # lists and other objects should compare just fine
+        else:
+            return minimalState == actualState
+
+    def Set(self, setUrl, httpMethod, minimalState):
+
+        try:
+            # Foreman's API specifies that put and post api calls must set the Content-type header
+            # If we dont, we will get an exception as follows:
+            #       Exception.args[0]:
+            #           '415 Client Error: Unsupported Media Type for url: https://15.4.7.1/api/environments'
+            #
+            #       response._content:
+            #           b'{\n  "error": {"message":"\'Content-Type: \' is unsupported in API v2 for POST and PUT requests. Please use \'Content-Type: application/json\'."}\n}\n'
+
+            headers = {}
+            if httpMethod.lower() in ["put", "post"]:
+                headers = {'Content-type': 'application/json'}
+
+            actualState = self.apiWrapper.MakeApiCall(setUrl, httpMethod, minimalState, headers)
+
+            minimalStateExists = self.Compare(minimalState, actualState)
+
+            if not minimalState:
+                raise ApiStateEnforcerException(self.SetCompareFailedErrorMessage, minimalState, actualState)
+        except Exception as e:
+            raise Exception("An error occurred while setting state.") from e
+
+
